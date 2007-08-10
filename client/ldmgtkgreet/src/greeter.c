b4071b1513e06f8859f765d0ea2396fe61f81992
Scott Balneaves <sbalneav@edubuntu>
2007-08-10 00:27:13 -0500

    Added session window

--- client/ldmgtkgreet/src/greeter.c
+++ client/ldmgtkgreet/src/greeter.c
@@ -44,6 +44,9 @@ GtkWidget *entry;                           /* entry box */
 GtkWidget *lang_select;                     /* language selection combo */
 gint lang_total = 0;
 
+GtkWidget *sess_select;                     /* session selection combo */
+gint sess_total = 0;
+
 GHashTable *ldminfo_hash = NULL;
 GList *sorted_host_list = NULL;
 gint current_host_id = 0;
@@ -74,6 +77,13 @@ populate_lang_combo_box(const char *lang, GtkWidget *lang_combo_box)
 {
 	gtk_combo_box_append_text(GTK_COMBO_BOX(lang_combo_box),g_strdup(lang));
     lang_total++;
+}
+
+static void
+populate_sess_combo_box(const char *sess, GtkWidget *sess_combo_box)
+{
+	gtk_combo_box_append_text(GTK_COMBO_BOX(sess_combo_box),g_strdup(sess));
+    sess_total++;
 }
 
 gboolean
@@ -193,63 +203,69 @@ handle_command(GIOChannel *io_input)
 static void
 sesswin(GtkWidget *widget, GtkWindow *win)
 {
-	GtkWidget *sesswin, *s_vbox, *s_buttonbox;
-	GtkWidget *s_cancel, *s_accept, *s_frame, *s_vbox2;
-	GtkWidget *radio_button1;
-	GtkWidget *radio_button2;
-	/*GtkWidget *radio_button3;*/
+	GtkWidget *sesswin, *label, *vbox, *buttonbox;
+	GtkWidget *cancel, *accept, *frame;
+    ldminfo *curr_host = NULL;
+	
+	sess_select = gtk_combo_box_new_text();
+
+    /* 
+     * Populate sess with default host hash
+     */
+
+    curr_host = g_hash_table_lookup(ldminfo_hash, 
+                                    g_list_nth_data(sorted_host_list,
+                                    current_host_id));
+
+    gtk_combo_box_append_text(GTK_COMBO_BOX(sess_select),
+                              g_strdup(_("Default")));
+    gtk_combo_box_append_text(GTK_COMBO_BOX(sess_select),
+                              g_strdup(_("Failsafe xterm")));
+    g_list_foreach(curr_host->sessions, 
+                   (GFunc)populate_sess_combo_box, sess_select);
+
+    gtk_combo_box_set_active(GTK_COMBO_BOX(sess_select), 0);
+
+    /*
+     * Build window
+     */
 
 	sesswin = gtk_window_new(GTK_WINDOW_POPUP);
 	gtk_window_set_position((GtkWindow *) sesswin, GTK_WIN_POS_CENTER_ALWAYS);
 	gtk_window_set_modal((GtkWindow *) sesswin, TRUE);
+	
+	vbox = gtk_vbox_new(FALSE, 0);
+	buttonbox = gtk_hbox_new(FALSE, 5);
+	gtk_container_set_border_width(GTK_CONTAINER(vbox), 5);
 
-	s_vbox = gtk_vbox_new(FALSE, 0);
-	s_vbox2 = gtk_vbox_new(FALSE, 0);
-	s_buttonbox = gtk_hbox_new(FALSE, 5);
+	cancel = gtk_button_new_from_stock("gtk-cancel");
+	g_signal_connect (G_OBJECT (cancel), "clicked",
+			G_CALLBACK (destroy_popup),
+			sesswin);
 
-	radio_button1 = gtk_radio_button_new_with_mnemonic( NULL, _("_1. Default") );
-	radio_button2 = gtk_radio_button_new_with_mnemonic( gtk_radio_button_group( GTK_RADIO_BUTTON( radio_button1 )), 
-				_("_2. Terminal (failsafe)") );
-	/*radio_button3 = gtk_radio_button_new_with_mnemonic(
-			gtk_radio_button_group( GTK_RADIO_BUTTON( radio_button1 )),
-				_("_3. GNOME") );*/
+	accept = gtk_button_new_with_mnemonic(_("Change _Session"));
 
-	gtk_box_pack_start(GTK_BOX(s_vbox2), GTK_WIDGET(radio_button1),
-                       FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(s_vbox2), GTK_WIDGET(radio_button2),
-                       FALSE, FALSE, 0);
-	/*gtk_box_pack_start(GTK_BOX(s_vbox2), GTK_WIDGET(radio_button3),
-                         FALSE, FALSE, 0);*/
+	gtk_box_pack_end((GtkBox *)buttonbox, (GtkWidget *)accept, FALSE, FALSE, 0);
+	gtk_box_pack_end((GtkBox *)buttonbox, (GtkWidget *)cancel, FALSE, FALSE, 0);
 
-	gtk_container_set_border_width (GTK_CONTAINER (s_vbox2), 15);
-	gtk_container_set_border_width (GTK_CONTAINER (s_vbox), 5);
+	label = gtk_label_new("");
+	gtk_label_set_markup((GtkLabel *) label, 
+			 _("Select your session manager:"));
 
-	s_cancel = gtk_button_new_from_stock("gtk-cancel");
-	g_signal_connect (G_OBJECT(s_cancel), "clicked", G_CALLBACK(destroy_popup),
-			          sesswin);
+	gtk_box_pack_start((GtkBox *)vbox, (GtkWidget *)label, FALSE, FALSE, 0);
+	gtk_box_pack_start((GtkBox *)vbox, (GtkWidget *)sess_select, FALSE, FALSE, 5);
+	gtk_box_pack_start((GtkBox *)vbox, (GtkWidget *)buttonbox, TRUE, TRUE, 5);
 
-	s_accept = gtk_button_new_with_mnemonic(_("Change _Session"));
+	frame = gtk_frame_new("");
+	gtk_frame_set_shadow_type((GtkFrame *)frame, GTK_SHADOW_OUT);
+	gtk_frame_set_label_align((GtkFrame *)frame, 1.0, 0.0);
 
-	gtk_box_pack_end(GTK_BOX(s_buttonbox), GTK_WIDGET(s_accept),
-                     FALSE, FALSE, 0);
-	gtk_box_pack_end(GTK_BOX(s_buttonbox), GTK_WIDGET(s_cancel),
-                     FALSE, FALSE, 0);
-
-	gtk_box_pack_start(GTK_BOX(s_vbox), GTK_WIDGET(s_vbox2), FALSE, FALSE, 0);
-	gtk_box_pack_start(GTK_BOX(s_vbox), GTK_WIDGET(s_buttonbox), TRUE, TRUE, 5);
-
-	s_frame = gtk_frame_new("");
-	gtk_label_set_markup(GTK_LABEL(gtk_frame_get_label_widget(GTK_FRAME(s_frame))),
-			_("<b>Sessions</b>"));
-	gtk_frame_set_shadow_type(GTK_FRAME(s_frame), GTK_SHADOW_OUT);
-	gtk_frame_set_label_align(GTK_FRAME(s_frame), 0.0, 0.0);
-
-	gtk_container_add (GTK_CONTAINER (s_frame), s_vbox);
-	gtk_container_add (GTK_CONTAINER (sesswin), s_frame);
+	gtk_container_add (GTK_CONTAINER(frame), vbox);
+	gtk_container_add (GTK_CONTAINER(sesswin), frame);
 
 	gtk_widget_show_all(sesswin);
-	return;
 
+	return;
 }
 
 
@@ -270,7 +286,8 @@ langwin(GtkWidget *widget, GtkWindow *win)
                                     g_list_nth_data(sorted_host_list,
                                     current_host_id));
 
-    gtk_combo_box_append_text(GTK_COMBO_BOX(lang_select), g_strdup(_("Default")));
+    gtk_combo_box_append_text(GTK_COMBO_BOX(lang_select),
+                              g_strdup(_("Default")));
     g_list_foreach(curr_host->languages, 
                    (GFunc)populate_lang_combo_box, lang_select);
 
@@ -295,23 +312,23 @@ langwin(GtkWidget *widget, GtkWindow *win)
 
 	accept = gtk_button_new_with_mnemonic(_("Change _Language"));
 
-	gtk_box_pack_end((GtkBox *) buttonbox, (GtkWidget *) accept, FALSE, FALSE, 0);
-	gtk_box_pack_end((GtkBox *) buttonbox, (GtkWidget *) cancel, FALSE, FALSE, 0);
+	gtk_box_pack_end((GtkBox *)buttonbox, (GtkWidget *)accept, FALSE, FALSE, 0);
+	gtk_box_pack_end((GtkBox *)buttonbox, (GtkWidget *)cancel, FALSE, FALSE, 0);
 
 	label = gtk_label_new("");
 	gtk_label_set_markup((GtkLabel *) label, 
 			 _("Select the language for your session to use:"));
 
-	gtk_box_pack_start((GtkBox *) vbox, (GtkWidget *) label, FALSE, FALSE, 0);
-	gtk_box_pack_start((GtkBox *) vbox, (GtkWidget *) lang_select, FALSE, FALSE, 5);
-	gtk_box_pack_start((GtkBox *) vbox, (GtkWidget *) buttonbox, TRUE, TRUE, 5);
+	gtk_box_pack_start((GtkBox *)vbox, (GtkWidget *)label, FALSE, FALSE, 0);
+	gtk_box_pack_start((GtkBox *)vbox, (GtkWidget *)lang_select, FALSE, FALSE, 5);
+	gtk_box_pack_start((GtkBox *)vbox, (GtkWidget *)buttonbox, TRUE, TRUE, 5);
 
 	frame = gtk_frame_new("");
-	gtk_frame_set_shadow_type((GtkFrame *) frame, GTK_SHADOW_OUT);
-	gtk_frame_set_label_align((GtkFrame *) frame, 1.0, 0.0);
+	gtk_frame_set_shadow_type((GtkFrame *)frame, GTK_SHADOW_OUT);
+	gtk_frame_set_label_align((GtkFrame *)frame, 1.0, 0.0);
 
-	gtk_container_add (GTK_CONTAINER (frame), vbox);
-	gtk_container_add (GTK_CONTAINER (langwin), frame);
+	gtk_container_add (GTK_CONTAINER(frame), vbox);
+	gtk_container_add (GTK_CONTAINER(langwin), frame);
 
 	gtk_widget_show_all(langwin);
 
