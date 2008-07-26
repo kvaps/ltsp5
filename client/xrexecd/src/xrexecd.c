58fb7c9a70e739d175aa4519dab5c8fe3cb79208
sbalneav <sbalneav@vger>
2008-07-26 10:24:49 -0500

    Cleanup of xrexecd, glib, now to be started from ldm's rc.d mechanism

--- client/xrexecd/src/xrexecd.c
+++ client/xrexecd/src/xrexecd.c
@@ -11,13 +11,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <pwd.h>
+#include <glib.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <X11/Xproto.h>
@@ -26,19 +25,17 @@
 #include <X11/Xlib.h>
 
 
-#define ATOM_USERNAME "LTSP_USERNAME"
 #define ATOM_PROGRAM  "LTSP_COMMAND"
-#define LOCKFILE      "/var/run/xrexecd.pid"
 #define MIN_UID 500
 
 /*
  * Globals
  */
 
-Atom user_atom;                 /* Our communication atom */
 Atom prog_atom;                 /* Our communication atom */
 Display *dpy;                   /* Display */
 Window root;                    /* root window */
+struct passwd *pw;
 
 void
 signal_handler(sig)
@@ -48,139 +45,58 @@ signal_handler(sig)
 }
 
 void
-run_command(struct passwd *pw, char *command)
+setup_env()
 {
-    int i;
-    i = open("/dev/null", O_RDWR);               /* stdin */
-    dup(i);                                      /* stdout */
-    dup(i);                                      /* stderr */
-
-    /* set supplementary groups */
-    if (initgroups(pw->pw_name, pw->pw_gid) == -1) {
-        exit(1);
-    }
-
-    /* set gid */
-    if (setgid(pw->pw_gid) == -1) {
-        exit(1);
-    }
-
-    /* set uid */
-    if (setuid(pw->pw_uid) == -1) {
-        exit(1);
-    }
-
-    /* change to home dir */
-    if (chdir(pw->pw_dir) == -1) {
-        exit(1);
-    }
-
-    umask(027);
-
-    /* set $USER */
-    if (setenv("USER", pw->pw_name, 1) == -1) {
-        exit(1);
-    }
-
-    /* set $HOME */
-    if (setenv("HOME", pw->pw_dir, 1) == -1) {
-        exit(1);
-    }
-
-    /* set $SHELL */
-    if (setenv("SHELL", pw->pw_shell, 1) == -1) {
-        exit(1);
-    }
-
-    system(command);
-    exit(0);
+    /* Become a process group leader */
+    (void) setsid();
 }
 
-/*
- * domount: actually bindmounts path1 onto path2.
- */
-
-void
-atom_exec(struct passwd *pw, const char *command)
+int
+atom_exec(char *command)
 {
-    int status;
-    pid_t child;
+    gchar **argv = NULL;        /* To parse up command line */
+    GPid  pid;
+    GSpawnFlags flags = G_SPAWN_SEARCH_PATH;
 
-    child = fork();
-
-    if (child == 0) {
-        int i;
-        pid_t child1;
-
-        setsid();                                /* be process group leader */
-        for (i = getdtablesize(); i >= 0; i--)   /* close all open fd's */
-            close(i);
-
-        child1 = fork();			/* fork again */
-
-        if (child1 == 0)
-            run_command(pw, (char *)command);
-        else
-            exit(0);
-    } else if (child > 0) {
-        return;
-    } else if (child < 0) {
-        perror("Error: fork() failed");
-        exit(1);
+    if (!g_shell_parse_argv (command, NULL, &argv, NULL)) {
+        return FALSE;
     }
 
+    if (!g_spawn_async(pw->pw_dir, argv, NULL, flags, setup_env, NULL, &pid,
+        NULL)) {
+        return FALSE;
+    }
+    return TRUE;
 }
 
 void
 handle_data()
 {
     char *command;
-    char *username;
     Atom type;
     int format, res;
     unsigned long nitems, nleft;
-    struct passwd *pw;
 
-    res = XGetWindowProperty(dpy, root, user_atom, 0, 512, 0, XA_STRING,
-                             &type, &format, &nitems, &nleft,
-                             (unsigned char **) &username);
-
-    if (res != Success)
-        return;
-    if (type != XA_STRING) {
-        XFree(username);
-        return;
-    }
-
-    /*
-     * check password entry here
-     */
-
-    pw = getpwnam(username);
-    XFree(username);
-
-    if (!pw)
-        return;
-
-    if (pw->pw_uid < MIN_UID)                    /* only allow exec as unpriv user */
-        return;
 
     res = XGetWindowProperty(dpy, root, prog_atom, 0, 512, 0, XA_STRING,
                              &type, &format, &nitems, &nleft,
                              (unsigned char **) &command);
 
-    if (res != Success)
+    if (res != Success) {
         return;
-    if (type != XA_STRING) {
+    } else if (type != XA_STRING) {
         XFree(command);
         return;
     }
 
     /*
-     * We now have a string (username) and a string (command).
+     * We now have a string (command).
      */
 
-    atom_exec(pw, command);
+    if (atom_exec(command) == FALSE) {
+        fprintf(stderr, "Couldn't execute.\n");
+        /* syslog something here */
+    }
     XFree(command);                              /* free up returned data */
 }
 
@@ -188,20 +104,63 @@ int
 main(int argc, char *argv[])
 {
     XEvent ev;
-    char *d = getenv("DISPLAY");
-    char str[10];
-    int lockfile;
+    gchar *d = getenv("DISPLAY");
+    gchar *username = getenv("LDM_USERNAME");	/* passed by the ldm rc.d env */
+
+    /*
+     * check password entry here
+     */
+
+    pw = getpwnam(username);
+
+    if (!pw) {
+        exit(1);
+    }
+
+    if (pw->pw_uid < MIN_UID) {         /* only allow exec as unpriv user */
+        exit(1);
+    }
+
+    /*
+     * We have a valid LDM_USERNAME that we have a valid passwd entry for.
+     */
 
     daemon(0,0);
 
-    lockfile = open(LOCKFILE, O_RDWR|O_CREAT, 0640);
-    if (lockfile < 0)
-        exit(1);
-    if (lockf(lockfile, F_TLOCK, 0) < 0)
-	exit(1);
-    sprintf(str, "%d\n", getpid());
-    write(lockfile, str, strlen(str));
+    /* set $USER */
+    if (setenv("USER", pw->pw_name, 1) == -1) {
+        return;
+    }
 
+    /* set $USERNAME */
+    if (setenv("USERNAME", pw->pw_name, 1) == -1) {
+        return;
+    }
+
+    /* set $HOME */
+    if (setenv("HOME", pw->pw_dir, 1) == -1) {
+        return;
+    }
+
+    /* set $SHELL */
+    if (setenv("SHELL", pw->pw_shell, 1) == -1) {
+        return;
+    }
+
+    /* set supplementary groups */
+    if (initgroups(pw->pw_name, pw->pw_gid) == -1) {
+        return FALSE;
+    }
+
+    /* set gid */
+    if (setgid(pw->pw_gid) == -1) {
+        return FALSE;
+    }
+
+    /* set uid */
+    if (setuid(pw->pw_uid) == -1) {
+        return FALSE;
+    }
 
     /*
      * Open our display
@@ -222,16 +181,10 @@ main(int argc, char *argv[])
     root = RootWindow(dpy, DefaultScreen(dpy));
 
     /*
-     * We've got the root window now.  First, register our needed atoms.
+     * We've got the root window now.  First, register our needed atom.
      */
 
-    user_atom = XInternAtom(dpy, ATOM_USERNAME, False);
     prog_atom = XInternAtom(dpy, ATOM_PROGRAM, False);
-
-    if (user_atom == None) {
-        printf("Couldn't allocate atom for %s\n", ATOM_USERNAME);
-        exit(1);
-    }
 
     if (prog_atom == None) {
         printf("Couldn't allocate atom for %s\n", ATOM_PROGRAM);
@@ -239,7 +192,7 @@ main(int argc, char *argv[])
     }
 
     /*
-     * declare our interest in obtaining PropertyChange events.
+     * Declare our interest in obtaining PropertyChange events.
      */
 
     XSelectInput(dpy, root, PropertyChangeMask);
