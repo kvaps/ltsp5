3c5ccd58f437c7bce6945e09574e0f1643523d3c
Warren Togami <wtogami@redhat.com>
2008-08-14 15:16:38 -0400

    Fedora: Also use wraplinux to create NBI and ELF images.        mknbi's NBI image isn't working for me, while wraplinux's NBI seems to work.        I plan on doing more testing to see which to keep...

--- server/configs/k12linux/mkinitrd/ltsp-postinst.d
+++ server/configs/k12linux/mkinitrd/ltsp-postinst.d
@@ -4,7 +4,7 @@
 [ ! -e /etc/ltsp_chroot ] && exit 0
 KERNELOPTS="ro quiet selinux=0 rhgb"
 
-# Image for ELF and coreboot
+# Image for ELF and coreboot, Etherboot-5.4
 if [ -x /usr/sbin/mkelfImage ]; then
   rm -f /boot/elf-$1.img
   mkelfImage --kernel=/boot/vmlinuz-$1 --initrd=/boot/initrd-$1.img --output=/boot/elf-$1.img --append="$KERNELOPTS"
@@ -14,8 +14,22 @@ fi
 # Image for really old NBI-only clients
 if [ -x /usr/bin/mknbi-linux ]; then
   rm -f /boot/legacy-nbi-$1.img
-  mknbi-linux /boot/vmlinuz-$1 /boot/initrd-$1.img --output=/boot/legacy-nbi-$1.img --append="$KERNELOPTS"
+  mknbi-linux /boot/vmlinuz-$1 /boot/initrd-$1.img --output=/boot/legacy-nbi-$1.img --append="$KERNELOPTS" 2> /dev/null
   ln -sf legacy-nbi-$1.img /boot/legacy-nbi.ltsp
+fi
+
+# Wraplinux NBI
+if [ -x /usr/bin/wraplinux ]; then
+  rm -f /boot/wraplinux-nbi-$1.img
+  wraplinux --nbi /boot/vmlinuz-$1 --initrd /boot/initrd-$1.img -o /boot/wraplinux-nbi-$1.img
+  ln -sf wraplinux-nbi-$1.img /boot/wraplinux-nbi.ltsp
+fi
+
+# Wraplinux ELF
+if [ -x /usr/bin/wraplinux ]; then
+  rm -f /boot/wraplinux-elf-$1.img
+  wraplinux --elf /boot/vmlinuz-$1 --initrd /boot/initrd-$1.img -o /boot/wraplinux-elf-$1.img
+  ln -sf wraplinux-elf-$1.img /boot/wraplinux-elf.ltsp
 fi
 
 # Symlink vmlinuz.ltsp and initrd.ltsp and set permissions for tftp server
