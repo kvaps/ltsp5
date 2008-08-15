c93cc9e7cd6aa5e2788ccc584e5be60970d70f89
Warren Togami <wtogami@redhat.com>
2008-08-14 22:37:44 -0400

    Fedora: Standardize on wraplinux for NBI, and mkelfimage for ELF images.         Defaults added to Fedora's /etc/ltsp/dhcpd.conf.

--- server/configs/k12linux/mkinitrd/ltsp-postinst.d
+++ server/configs/k12linux/mkinitrd/ltsp-postinst.d
@@ -5,17 +5,11 @@
 KERNELOPTS="ro quiet selinux=0 rhgb"
 
 # Image for ELF and coreboot, Etherboot-5.4
+# Not using wraplinux for ELF because it cannot boot on coreboot.
 if [ -x /usr/sbin/mkelfImage ]; then
   rm -f /boot/elf-$1.img
   mkelfImage --kernel=/boot/vmlinuz-$1 --initrd=/boot/initrd-$1.img --output=/boot/elf-$1.img --append="$KERNELOPTS"
   ln -sf elf-$1.img /boot/elf.ltsp
-fi
-
-# Image for really old NBI-only clients
-if [ -x /usr/bin/mknbi-linux ]; then
-  rm -f /boot/legacy-nbi-$1.img
-  mknbi-linux /boot/vmlinuz-$1 /boot/initrd-$1.img --output=/boot/legacy-nbi-$1.img --append="$KERNELOPTS" 2> /dev/null
-  ln -sf legacy-nbi-$1.img /boot/legacy-nbi.ltsp
 fi
 
 # Wraplinux NBI
@@ -23,13 +17,6 @@ if [ -x /usr/bin/wraplinux ]; then
   rm -f /boot/wraplinux-nbi-$1.img
   wraplinux --nbi /boot/vmlinuz-$1 --initrd /boot/initrd-$1.img -o /boot/wraplinux-nbi-$1.img
   ln -sf wraplinux-nbi-$1.img /boot/wraplinux-nbi.ltsp
-fi
-
-# Wraplinux ELF
-if [ -x /usr/bin/wraplinux ]; then
-  rm -f /boot/wraplinux-elf-$1.img
-  wraplinux --elf /boot/vmlinuz-$1 --initrd /boot/initrd-$1.img -o /boot/wraplinux-elf-$1.img
-  ln -sf wraplinux-elf-$1.img /boot/wraplinux-elf.ltsp
 fi
 
 # Symlink vmlinuz.ltsp and initrd.ltsp and set permissions for tftp server
