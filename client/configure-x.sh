ca5134ea3537364db08a4aa85f1f9dbdf1c2e981
Oliver Grawert <ogra@ubuntu.com>
2009-01-31 11:50:00 +0100

    add proper copyright header to configure-x.sh (all people were added based on best knowledge and code commits, please add yourself or people you committed code for in case i forgot someone)

--- client/configure-x.sh
+++ client/configure-x.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+#
+#  Copyright (c) 2007 Canonical LTD
+#
+#  Author: Oliver Grawert <ogra@canonical.com>
+#
+#  2007, Scott Balneaves <sbalneav@ltsp.org>
+#        Vagrant Cascadian <vagrant@freegeek.org>
+#        Gideon Romm <ltsp@symbio-technologies.com>
+#  2008, Oliver Grawert <ogra@canonical.com>
+#        Eric Harrison <eharrison@k12linux.mesd.k12.or.us>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
 
 #
 # If someone's overridden the config with their own custom config, then
