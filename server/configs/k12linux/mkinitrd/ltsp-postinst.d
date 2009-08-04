4384aa0ba3c8ba3c6e19155fadf5760a2b6e3833
Warren Togami <wtogami@redhat.com>
2009-08-04 15:11:14 -0400

    Fedora: This should fully handle generic initrd wrapping too

--- server/configs/k12linux/mkinitrd/ltsp-postinst.d
+++ server/configs/k12linux/mkinitrd/ltsp-postinst.d
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ -f /boot/initrd-generic-$1.img ]; then
+    INITRD=initrd-generic-$1.img
+    rm -f /boot/initrd-$1.img
+else
+    INITRD=initrd-$1.img
+fi
 
 # Prep kernel and initrd for various types of LTSP netboot
 [ ! -e /etc/ltsp_chroot ] && exit 0
@@ -8,14 +14,14 @@ KERNELOPTS="ro quiet selinux=0 rhgb"
 # Not using wraplinux for ELF because it cannot boot on coreboot.
 if [ -x /usr/sbin/mkelfImage ]; then
   rm -f /boot/elf-$1.img
-  /usr/sbin/mkelfImage --kernel=/boot/vmlinuz-$1 --initrd=/boot/initrd-$1.img --output=/boot/elf-$1.img --append="$KERNELOPTS"
+  /usr/sbin/mkelfImage --kernel=/boot/vmlinuz-$1 --initrd=/boot/$INITRD --output=/boot/elf-$1.img --append="$KERNELOPTS"
   ln -sf elf-$1.img /boot/elf.ltsp
 fi
 
 # Wraplinux NBI
 if [ -x /usr/bin/wraplinux ]; then
   rm -f /boot/wraplinux-nbi-$1.img
-  /usr/bin/wraplinux --nbi /boot/vmlinuz-$1 --initrd /boot/initrd-$1.img -o /boot/wraplinux-nbi-$1.img
+  /usr/bin/wraplinux --nbi /boot/vmlinuz-$1 --initrd /boot/$INITRD -o /boot/wraplinux-nbi-$1.img
   ln -sf wraplinux-nbi-$1.img /boot/wraplinux-nbi.ltsp
 fi
 
@@ -35,16 +41,10 @@ if [ -x /usr/bin/piggyback64 ]; then
   else
     PIGGY=/usr/bin/piggyback
   fi
-  $PIGGY /boot/aout-$1 /boot/System.map-$1 /boot/initrd-$1.img
+  $PIGGY /boot/aout-$1 /boot/System.map-$1 /boot/$INITRD
 fi
 
 # Symlink vmlinuz.ltsp and initrd.ltsp and set permissions for tftp server
 ln -sf vmlinuz-$1    /boot/vmlinuz.ltsp
-if [ -f /boot/initrd-generic-$1.img ]; then
-  # dracut initrd
-  ln -sf initrd-generic-$1.img /boot/initrd.ltsp
-  rm /boot/initrd-$1.img
-else
-  ln -sf initrd-$1.img /boot/initrd.ltsp
-fi
+ln -sf $INITRD /boot/initrd.ltsp
 chmod 644 /boot/initrd-*$1.img
